e652a6acfd17fcd46fe0cd2e55b4fea8495228c7
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-06-17 18:21:35 +0900

    refactor : aws service & .env config

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,16 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    SERVER_URL: string;
+    NODE_ENV: string;
     PORT: number;
+    DATABASE_URL: string;
     ADMIN_NAME: string;
     ADMIN_PASSWORD: string;
+    JWT_SECRET_KEY: string;
+    AWS_ACCESS_KEY_ID: string;
+    AWS_SECRET_ACCESS_KEY: string;
+    AWS_REGION: string;
     AWS_S3_BUCKET: string;
+    AWS_CLOUDFRONT_DOMAIN: string;
   }
 }
--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -1,95 +1,50 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import AWS from 'aws-sdk';
+import { S3 } from '@aws-sdk/client-s3';
+import { Upload } from '@aws-sdk/lib-storage';
+import dayjs from 'dayjs';
+
+const { AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_BUCKET } = process.env;
 
 @Injectable()
 export class AwsService {
-  private readonly s3: AWS.S3;
+  private readonly s3: S3;
 
   constructor() {
-    this.s3 = new AWS.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: process.env.AWS_REGION,
+    this.s3 = new S3({
+      region: AWS_REGION,
+      credentials: { accessKeyId: AWS_ACCESS_KEY_ID, secretAccessKey: AWS_SECRET_ACCESS_KEY },
     });
   }
 
-  async uploadImage(
-    images: Array<Express.Multer.File>,
-    fisrtDist: string,
-    id: number,
-    secondDist: string = '',
-  ) {
-    const promises = images.map(async (image) => {
-      const params = {
-        Bucket: process.env.AWS_S3_BUCKET,
-        Key: `${fisrtDist}/${id}/${secondDist}${secondDist ? '/' : ''}${Date.now()}`,
-        Body: image.buffer,
-        ContentType: image.mimetype,
-      };
-
-      return await this.s3.putObject(params).promise();
+  async findImages(prefix: string) {
+    const objects = await this.s3.listObjectsV2({
+      Bucket: AWS_S3_BUCKET,
+      Prefix: prefix,
     });
 
+    return objects.Contents && objects.Contents.length > 0 ? objects.Contents : null;
+  }
+
+  async uploadImages(images: Express.Multer.File[], firstDist: string, id: number, secondDist: string = '') {
     try {
+      const promises = images.map(async (image, index) => {
+        const params = {
+          Bucket: AWS_S3_BUCKET,
+          Key: `${firstDist}/${id}/${secondDist}${secondDist ? '/' : ''}${dayjs().unix()}_${index}`,
+          Body: image.buffer,
+          ContentType: image.mimetype,
+        };
+
+        return await new Upload({ client: this.s3, params }).done();
+      });
+
       await Promise.all(promises);
+
+      return `${firstDist}/${id}/${secondDist}${secondDist ? '/' : ''}`;
     } catch (e) {
       console.error(e);
 
       throw new InternalServerErrorException();
-    }
-
-    return `${fisrtDist}/${id}/${secondDist}${secondDist ? '/' : ''}`;
-  }
-
-  async findImage(prefix: string) {
-    const objects = await this.s3
-      .listObjectsV2({
-        Bucket: process.env.AWS_S3_BUCKET,
-        Prefix: prefix,
-      })
-      .promise();
-
-    return objects.Contents && objects.Contents.length > 0
-      ? `${process.env.AWS_CLOUDFRONT_DOMAIN}/${objects.Contents[0].Key}`
-      : '';
-  }
-
-  async findImages(prefix: string) {
-    const objects = await this.s3
-      .listObjectsV2({
-        Bucket: process.env.AWS_S3_BUCKET,
-        Prefix: prefix,
-      })
-      .promise();
-
-    if (objects.Contents) {
-      return objects.Contents.map((object) => {
-        return `${process.env.AWS_CLOUDFRONT_DOMAIN}/${object.Key}`;
-      });
-    }
-
-    return [];
-  }
-
-  async deleteImages(firstDist: string, id: number, secondDist: string = '') {
-    const objects = await this.s3
-      .listObjectsV2({
-        Bucket: process.env.AWS_S3_BUCKET,
-        Prefix: `${firstDist}/${id}${secondDist ? '/' : ''}${secondDist}`,
-      })
-      .promise();
-
-    if (objects.Contents) {
-      const promises = objects.Contents.map(async (object) => {
-        const params = object.Key && {
-          Bucket: process.env.AWS_S3_BUCKET,
-          Key: object.Key,
-        };
-
-        return params && (await this.s3.deleteObject(params).promise());
-      });
-
-      Promise.all(promises);
     }
   }
 }
