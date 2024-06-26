c453172d04efc8c98badd57051c1aa56d4c54cf8
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-06-26 16:05:18 +0900

    refactor : profiles module

--- src/profiles/profiles.module.ts
+++ src/profiles/profiles.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ProfilesController } from './profiles.controller';
 import { ProfilesService } from './profiles.service';
+import { ProfilesRepository } from './profiles.repository';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ProfilesController],
-  providers: [ProfilesService],
+  providers: [ProfilesService, ProfilesRepository],
 })
 export class ProfilesModule {}
