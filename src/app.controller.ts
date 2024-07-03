9614cfcecf73ff6855d31011d699b01bf18727ee
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-07-03 17:55:37 +0900

    update : index.hbs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
 
+const { AWS_CLOUDFRONT_DOMAIN } = process.env;
+
 @ApiExcludeController()
 @Controller('/')
 export class AppController {
@@ -8,6 +10,7 @@ export class AppController {
   @Render('index.hbs')
   async renderIndex() {
     return {
+      awsCloudDomain: AWS_CLOUDFRONT_DOMAIN,
       instagramId: 'hjhj_9292',
     };
   }
