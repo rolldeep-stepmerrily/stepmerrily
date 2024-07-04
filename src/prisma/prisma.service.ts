70b5f72b5feeb43b5dd461411eef182764c7975b
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-07-04 09:59:45 +0900

    refactor : prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+const { NODE_ENV } = process.env;
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    super({
-      log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['warn', 'error'],
-    });
+    super({ log: NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['warn', 'error'] });
   }
 
   async onModuleInit() {
