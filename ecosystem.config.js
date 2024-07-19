21759699ac5471f05cbd33166b47d536675bb932
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-07-19 09:03:06 +0900

    update : pm2 cluster mode

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'stepmerrily',
+      script: 'dist/main.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
